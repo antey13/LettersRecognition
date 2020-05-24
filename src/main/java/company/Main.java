52e3b06729a2377a571c92d03ccd1b1c9e973e24
antey13 <shelderdragon@gmail.com>
2020-05-24 20:07:34 +0300

    refactor

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -3,14 +3,13 @@ package company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter text line from A,L and spaces");
         String next = reader.readLine();
         int[][] ints = new ImageUtil().generateImage(next.toCharArray());
         final List<Character> recognize = new TextRecognition(ints).recognize();
